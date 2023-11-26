adeafdccd9441bdf4b799623dc6b5ca7fe3aeab7
kanzamaadan <kenzamaadan@gmail.com>
2023-11-25 23:39:12 -0800

    0x05-processes_and_signals

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,24 +3,23 @@
 #include <unistd.h>
 
 int infinite_while(void) {
-    while (1) {
-        sleep(1);
-    }
-    return (0);
+while (1) {
+sleep(1);
+}
+return (0);
 }
 
 int main(void) {
-    int i;
-    pid_t zombie_pid;
+int i;
+pid_t zombie_pid;
 
-    for (i = 0; i < 5; i++) {
-        zombie_pid = fork();
-        if (zombie_pid == 0) {
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0);
-        }
-    }
-
-    infinite_while();
-    return (0);
+for (i = 0; i < 5; i++) {
+zombie_pid = fork();
+if (zombie_pid == 0) {
+printf("Zombie process created, PID: %d\n", getpid());
+exit(0);
+}
+}
+infinite_while();
+return (0);
 }
